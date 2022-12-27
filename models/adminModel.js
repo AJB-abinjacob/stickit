089f8f889e2060060f09f2261c039ef86c602c50
Umar Muqthar <umarnmuqthar@gmail.com>
2022-12-27 06:12:07 +0530

    removed constructor from admin model

--- models/adminModel.js
+++ models/adminModel.js
@@ -2,20 +2,6 @@ const { ObjectId } = require('mongodb')
 const getDb = require('../utils/database').getDb
 
 class Admin {
-  constructor (email, password) {
-    this.name = 'Stickit Admin'
-    this.email = email
-    this.password = password
-    this.active = true
-    this.createdOn = new Date()
-    this.updatedOn = new Date()
-  }
-
-  save () {
-    const db = getDb()
-    return db.collection('admins').insertOne(this)
-  }
-
   static findById (id) {
     const db = getDb()
     return db
